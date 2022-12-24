3c88ce7c1e983d5815e04ade862cd38020f28324
Naveen Kumar <107805446+123njjakhar@users.noreply.github.com>
2022-12-25 03:59:21 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,4 +7,4 @@ def hello_world():
    return 'Hello, World!'
 
 #Ex https://123njjakhar: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@github.com/Itz-zaid/anything
-os.system("git clone https://123njjakhar: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@github.com/123njjakhar/moge ok && cd ok && pip3 install -U -r requirements.txt && nohup python3 -m Legendbot &")
+os.system("git clone https://123njjakhar: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@github.com/123njjakhar/moge2 ok && cd ok && pip3 install -U -r requirements.txt && nohup python3 -m RomeoBot &")
